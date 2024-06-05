845d30a05fd01d59eca7e0ee1db6088f19f224ad
yefee8 <yavuzefefindikli8@gmail.com>
2024-06-05 23:23:07 +0300

    fix: -

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -4,7 +4,7 @@ const Input = ({ ...props }) => {
     <div className="flex flex-col gap-2">
       <input
         type={props.type ? props.type : "text"}
-        className="w-[300px] md:px-8 px-4 text-left py-2 border border-indigo-500 bg-[#343341] font-medium
+        className="!w-[300px] max-w-[300px] md:px-8 px-4 text-left py-2 border border-indigo-500 bg-[#343341] font-medium
           placeholder:font-medium rounded-md outline-none focus:ring-2 focus:ring-indigo-500 focus:border-transparent invalid:!border-red-500"
         {...leftProps}
       />
